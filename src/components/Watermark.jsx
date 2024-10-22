83919d05c5df6ae918f3ab46a4e8a3b5661d2e14
ANURAG ARYAN <122167816+i-akb25@users.noreply.github.com>
2024-10-23 02:51:55 +0530

    Update Watermark.jsx

--- src/components/Watermark.jsx
+++ src/components/Watermark.jsx
@@ -4,7 +4,7 @@ import akbImage from "../assets/akb-image.png"; // Ensure this path is correct
 const Watermark = () => {
   return (
     <div className="watermark-container">
-      <button className="watermark-text">copyright @AKB</button>
+      <button className="watermark-text">copyright @akb</button>
       <img className="popup-image" src={akbImage} alt="AKB Image" />
     </div>
   );
